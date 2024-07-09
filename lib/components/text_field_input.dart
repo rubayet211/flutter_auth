946558d6a77619e31f27ef05332a72887926ebad
Rhyme Rubayet <rubayet211@gmail.com>
2024-07-09 18:02:15 +0600

    Login page update and custom text field added.

---
+++ lib/components/text_field_input.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class TextFieldInput extends StatelessWidget {
+  final String hintText;
+  final TextEditingController controller;
+  final bool obscureText;
+
+  const TextFieldInput(
+      {super.key,
+      required this.hintText,
+      required this.controller,
+      required this.obscureText});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        hintText: hintText,
+      ),
+    );
+  }
+}
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,6 +5,27 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.person,
+              size: 80,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Text(
+              "AUTH NOTE",
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
